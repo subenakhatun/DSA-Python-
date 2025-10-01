441c4dea6a2b0b842cbc043e394032ce1d9b6455
Subena khatun <subenakhtun@gmail.com>
2025-10-01 14:19:24 +0000

    define al algorithm

--- linear-search.py
+++ linear-search.py
@@ -1,1 +1,12 @@
+# linear Search algorithm
 
+"""
+Work Flow:
+1. Define a array or list
+2. Difne a targer value what you want to search or find from this list
+3. Make a fucntion linearSearch()
+4. apply codition for searching
+5. return function
+6. call function
+
+"""
