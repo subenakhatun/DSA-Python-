60909165645e8091b4152b319db71a1784afbe70
subenakhatun <subenakhatun@gmail.com>
2025-10-15 07:17:42 +0600

    binary search algorithm

--- binary-search.py
+++ binary-search.py
@@ -1,1 +1,13 @@
-print(10+20)
+li = [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150]
+target_value = 80
+start = 0
+end = len(li)-1
+print(len(li))
+print(end) 
+
+
+
+
+
+
+# result = binary_search(li,target_value)
