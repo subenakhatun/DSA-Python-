a9ed11d3674a2cbc6345851aaf32e159a7bc79c2
subenakhatun <subenakhatun@gmail.com>
2025-10-15 07:27:00 +0600

    binary search update code

--- binary-search.py
+++ binary-search.py
@@ -2,10 +2,17 @@ li = [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150]
 target_value = 80
 start = 0
 end = len(li)-1
-print(len(li))
-print(end) 
+# print(len(li))
+# print(end) 
+def binary_search(li,target_value):
+    while(start <= end):
 
-
+        mid = start+end//2
+        if mid == target_value:
+            return mid
+        elif mid < target_value:
+            start = mid +1
+            
 
 
 
